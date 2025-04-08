19273ef2663672188611ce39a2295389b27a0478
Sai Preetham <160500827+SaiPreetham23@users.noreply.github.com>
2025-04-08 21:57:01 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact us</h3>
+         <a href="tel:9639357063"><i class="fas fa-phone"></i> +91 9639357063</a>
+         <a href="tel:9357063963"><i class="fas fa-phone"></i> +91 9357063963</a>
+         <a href="mailto:pmpv@gmail.com"><i class="fas fa-envelope"></i> pmpv@gmail.com</a>
+         <a href="https://www.google.com"><i class="fas fa-map-marker-alt"></i> Ballari,Karnataka,India - 583101 </a>
+      </div>
+
+      <!-- <div class="box">
+         <h3>Follow us</h3>
+         <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+         <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="#"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div> -->
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Web Designers</span> | all rights reserved!</div>
+
+</footer>
